00f9ec4ba6e32aa916b2d42393bed63e06370979
NataliaGP007 <103333081+NataliaGP007@users.noreply.github.com>
2022-05-24 17:46:26 -0500

    Update SignoZodiacal.java

--- SignoZodiacal.java
+++ SignoZodiacal.java
@@ -1,17 +1,20 @@
 package ProyectoHerencia;
 
+// Superclase.
 public abstract class SignoZodiacal {
 
     protected String signoZodiacal;
     protected int mesNacimiento;
     protected int diaNacimiento;
 
+    // Constructor.
     public SignoZodiacal(int mesNacimiento, int diaNacimiento) {
         this.mesNacimiento = mesNacimiento;
         this.diaNacimiento = diaNacimiento;
 
     }
 
+    // Metodo abstracto.
     public abstract void identificarSignoZodiacal();
 
     public String getSignoZodiacal(){
